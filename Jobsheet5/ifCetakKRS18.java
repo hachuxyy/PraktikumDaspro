9d4da15aa5007df9334382c2b05b31fdb05e15b5
hachuxyy <msamsularifinb6@gail.com>
2025-10-02 07:20:19 +0700

    Percobaan 1

---
+++ Jobsheet5/ifCetakKRS18.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ifCetakKRS18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("----Cetak KRS Siakad----");
+        System.out.print("Apakah UKT Sudah Lunas? (True or False): ");
+        boolean uktLunas = input.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silahkan Cetak KRS dan Minta Tanda Tangan DPA");
+        }
+    }   
+}
