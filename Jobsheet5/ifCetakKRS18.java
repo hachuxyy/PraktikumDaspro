6c79762981ebdfc97575b8416c5a02228e7bcf31
hachuxyy <msamsularifinb6@gail.com>
2025-10-02 10:12:23 +0700

    Tugas 1

--- Jobsheet5/ifCetakKRS18.java
+++ Jobsheet5/ifCetakKRS18.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class ifCetakKRS18 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("----Cetak KRS Siakad----");
         System.out.print("Apakah UKT Sudah Lunas? (True or False): ");
-        boolean uktLunas = input.nextBoolean();
+        boolean uktLunas = sc.nextBoolean();
         String pesan;
 
         pesan = (uktLunas) ? "Pembayaran UKT Terverifikasi \n Silahkan Cetak KRS dan Minta Tanda Tangan DPA" : "Registrasi Ditolak, Silahkan Lunasi UKT Terlebih Dahulu";
